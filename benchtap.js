bed1233d98b357d520a8a35128102276f08394a5
Waylon Flinn <waylonflinn@gmail.com>
2016-11-25 10:09:00 -0600

    beginnings of transition to tape

--- benchtap.js
+++ benchtap.js
@@ -1,31 +1,125 @@
 var Benchmark = require('benchmark'),
-	createResult = require('tape/lib/results'),
-	createDefaultStream = require('tape/lib/default_stream');
+	test = require('tape');
 
-var results = createResult();
-var stream = results.createStream();
-stream.pipe(createDefaultStream());
-function write(s) { results._stream.queue(s) };
+// modified from tape/lib/test.js
+var getTestArgs = function (name_, opts_, setup_, test_) {
+	var name = '(anonymous)';
+	var opts = {};
+	var setup;
+	var test;
 
-var canEmitExit = typeof process !== 'undefined' && process
-	&& typeof process.on === 'function' && process.browser !== true
+	for (var i = 0; i < arguments.length; i++) {
+		var arg = arguments[i];
+		var t = typeof arg;
+		if (t === 'string') {
+			name = arg;
+		}
+		else if (t === 'object') {
+			opts = arg || opts;
+		}
+		else if (t === 'function') {
+			if(setup == null) setup = arg;
+			else test = arg;
+		}
+	}
+	return { name: name, opts: opts, setup: setup, test: test };
+};
 
-if(canEmitExit){
-	process.on('exit', function (code) {
-		// let the process exit cleanly.
-		if (code !== 0) return;
+var suite = new Benchmark.Suite();
 
-		results.close();
-		process.exit(code); //  || harness._exitCode
-	});
-} else {
-	results.once('done', function () { results.close() });
+// the percentage deviation to consider statistically significant
+SIGNIFICANT_ERROR = .3;
+
+var SI_PREFIXES = ["", "k", "M", "G", "T", "P", "E"];
+
+function getFlopsString(flops){
+
+	// what tier
+	var tier = Math.log10(flops) / 3 | 0;
+	var prefix = SI_PREFIXES[tier];
+	var scale = Math.pow(10, tier * 3);
+
+	var scaled = flops / scale;
+
+	// Benchmark.formatNumber(adjusted.toFixed(3)) + ' ' + prefix + 'Flops/sec '
+
+	return scaled.toFixed(3) + ' ' + prefix + 'Flops/sec';
 }
 
-/* Run benchmarks with tap output, compatible with testling and browserify
- */
+module.exports = function(name_, opts_, setup_, test_){
+
+	var args = getTestArgs(name_, opts_, setup_, test_);
+
+	var context = {};
+	var setup = args.setup.bind(context);
+	var test = args.test.bind(context);
+	var operations = args.opts.operations;
+
+/*
+	setup.bind(context)();
+	test.bind(context)();
+	*/
+	setup();
+
+	test(args.name, function(t){
+		t.plan(1);
+
+		var self = this;
+		var context = {};
+
+		var b = new Benchmark(name, test)
+			.on('start', setup)
+			.on('cycle', function(event) {
+			})
+			.on('complete', function(event) {
+				var benchmark = this;
+				if(benchmark.error){
+					t.fail(benchmark.error);
+				} else {
+
+					var pm = '\xb1',
+						mu = '\xb5'
+						size = benchmark.stats.sample.length;
+
+					var flops = benchmark.hz * operations;
+					flopsString = getFlopsString(flops);
+
+					var info = flopsString +
+						' ' + pm + benchmark.stats.rme.toFixed(2) + '% ' +
+						' n = ' + size +
+						' ' + mu + " = " + (benchmark.stats.mean * 1000).toFixed(0) + 'ms'
+
+					// flagged to run verbose?
+					if(self.verbose){
+						// show concise graphical representation
+						info += ' : ' + benchmark.stats.sample.map(function(s){
+							var error = (s - benchmark.stats.mean);
+							var threshold = SIGNIFICANT_ERROR*benchmark.stats.mean;
+							if(error < -threshold){
+								return ".";
+							} else if(error > threshold){
+								return "*"
+							}
+
+							return "-";
+						}).join('');
+					}
+
+
+					t.pass("passed");
+					t.comment(info);
+					// higher verbosity level?
+					if(self.verbose && self.verbose > 1){
+						t.comment('[' + benchmark.stats.sample + ']');
+					}
+				}
+			});
+
+			suite.add(b);
+	});
+};
+/*
 function Benchtap(verbose){
-	this.suite = new Benchmark.Suite();
 	this.pass = 0;
 	this.fail = 0;
 	var self = this;
@@ -38,16 +132,7 @@ function Benchtap(verbose){
 
 module.exports = Benchtap;
 
-// the percentage deviation to consider statistically significant
-Benchtap.SIGNIFICANT_ERROR = .3;
 
-/*
-	setup - function containing setup code
-		allocate data, create data structures, etc.
-	test - function containing test code
-	ops - function calculating number of operations run in the test (for flops)
-
- */
 Benchtap.prototype.add = function(name, setup, test, ops){
 
 	var self = this;
@@ -104,33 +189,13 @@ Benchtap.prototype.add = function(name, setup, test, ops){
 	this.suite.add(b);
 }
 
-var SI_PREFIXES = ["", "k", "M", "G", "T", "P", "E"];
-
-function getFlopsString(flops){
-
-	// what tier
-	var tier = Math.log10(flops) / 3 | 0;
-	var prefix = SI_PREFIXES[tier];
-	var scale = Math.pow(10, tier * 3);
-
-	var scaled = flops / scale;
-
-	// Benchmark.formatNumber(adjusted.toFixed(3)) + ' ' + prefix + 'Flops/sec '
-
-	return scaled.toFixed(3) + ' ' + prefix + 'Flops/sec';
-}
 
 Benchtap.prototype.run = function(){
-	/*
-	if(!harness.running){
-		harness.running = true;
-		printHeader();
-	}*/
 	results.count += this.suite.length;
 
 	this.suite.run({ 'async': true});
 }
-
+*/
 /*
 function printHeader(){
 	console.log("TAP version 13");
@@ -148,7 +213,7 @@ function printFooter(total, pass, fail){
 		console.log("\n# ok\n");
 }
 */
-
+/*
 function printError(id, name, error){
 	write("not ok " + id + " " + name + "\n");
 	// show error
@@ -161,3 +226,4 @@ function printPass(id, name, info){
 	write("ok " + id + " " + name + "\n");
 	write("# " + info + "\n");
 }
+*/
